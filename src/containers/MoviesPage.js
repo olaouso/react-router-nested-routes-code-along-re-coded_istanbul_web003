05db020609ffc653d464bfeee2293d39e9610266
Ola Ouso <ouso.ola@gmail.com>
2020-11-01 13:20:20 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -6,6 +6,7 @@ import MoviesList from '../components/MoviesList';
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
 )
 
